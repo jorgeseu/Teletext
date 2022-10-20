e564b0829efb3ff9ddcc3a67b4c64e7d0133cd2c
jorgeseu <adrian.kurlej@protonmail.com>
2022-10-20 16:46:40 +0000

    adding main app to project called core. Enabling html and js files

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+urlpatterns = [
+    path('', views.homepage),
+]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- teletext/settings.py
+++ teletext/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -118,8 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_FRONT_URL = '/frontend/static/'
+STATIC_ROOT = '/var/www/venv/src/static/'
+MEDIA_ROOT = '/var/www/venv/src/media/'
+STATIC_FRONT_ROOT = '/var/www/venv/src/frontend/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
