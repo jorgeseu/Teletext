18fa2171d58cdf7c52985bd376264ae9e9323d73
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2022-10-21 16:47:59 +0200

    weather app core

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from . import weatherview
 
 urlpatterns = [
     path('', views.homepage),
+    path('weather/', weatherview.weather_call),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
