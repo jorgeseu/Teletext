216efc724366fd1cc0116b138be98c57edd35c91
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2022-10-21 19:46:59 +0200

    weather app core js

--- teletext/settings.py
+++ teletext/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = 'core/templates/static/'
+MEDIA_URL = 'core/media/'
 STATIC_ROOT = '/var/www/teletext/teletext_core/teletext/teletext/core/templates/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
