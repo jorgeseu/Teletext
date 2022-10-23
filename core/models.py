370e79daf961ad80b851ef4da935fe97a61e4fb9
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2022-10-23 19:30:41 +0200

    weather app core js

--- core/models.py
+++ core/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db import models
-from oauth2client.contrib.django_util.models import CredentialsField
+#from oauth2client.contrib.django_util.models import CredentialsField
 
 # Create your models here.
 
-class CredentialsModel(models.Model):
-    id = models.ForeignKey(User, primary_key = True, on_delete = models.CASCADE)
-    credential = CredentialsField()
-    task - models.CharField(max_length = 80, null = True)
-    updated_time = models.CharField(max_length = 80, null = True)
+#class CredentialsModel(models.Model):
+#    id = models.ForeignKey(User, primary_key = True, on_delete = models.CASCADE)
+#    credential = CredentialsField()
+#    task - models.CharField(max_length = 80, null = True)
+#    updated_time = models.CharField(max_length = 80, null = True)
 
-class CredentialsAdmin(admin.ModelAdmin): # agora quem vai saber
-    pass
+#class CredentialsAdmin(admin.ModelAdmin): # agora quem vai saber
+#    pass
