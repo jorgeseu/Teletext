be302db921937c12215f3ef617e19750591d7197
jorgeseu <adrian.kurlej@protonmail.com>
2022-10-27 20:22:15 +0000

    adding vueproject files

---
+++ frontend/vue.config.js
@@ -0,0 +1,12 @@
+const pages = {  
+  index: "src/main.js",  
+};  
+  
+module.exports = {  
+  publicPath: "/static/vue/",
+  outputDir: "./build/static/vue/",
+  indexPath: "../../templates/vue_index.html",
+  
+  pages: pages,  
+  
+};
