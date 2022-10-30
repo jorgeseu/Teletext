840060d2bf751995775cdfefa621a106620c75c4
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2022-10-30 21:11:22 +0100

    vue js test

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-#random comment aqui
+
+#sem comentario
 def frontend(request):
   return HttpResponse(render(request, 'vue_index.html'))
