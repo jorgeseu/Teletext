5db251ee95ebf76f46dbea47a815507aa0c64368
mikolajsniechowski <mikolaj.sniechowski@stud.pwsz.legnica.edu.pl>
2022-10-31 18:47:57 +0100

    test

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      TEST<br>
+      
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
