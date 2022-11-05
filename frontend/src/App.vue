d83d2548e13e514a063cfae1ea0349405354b5e4
mikolajsniechowski <mikolaj.sniechowski@stud.pwsz.legnica.edu.pl>
2022-11-05 01:21:40 +0100

    Teletext mainpage protype

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav>
+    <router-link to="/">Telegazeta</router-link>
+  </nav>
+  <div class="container">
+    <div class="jumbotron">
+        <h1 class="telegazeta"> TELEGAZETA</h1>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Telegazeta Online</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Ogłoszenia</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">O nas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Rejestracja</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Logowanie</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <hr class="bg-primary">
+      <div class="row-fluid bg-primary">
+      <center><router-view/> </center>
+      </div>
+      <footer>
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    Nie wszystkie prawa zastrzeżone
+  </div>
+      </footer>
+ </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style src="@/assets/css/bootstrap.css" type="text/css">
 </style>
