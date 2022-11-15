6f473c8da1ddac5760c7de0c349a580d24eae4f1
Chris <kbalicki2014@gmail.com>
2022-11-15 01:12:09 +0100

    test fix

---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from rest_framework.parsers import JSONParser
+# To bypass having a CSRF token
+from django.views.decorators.csrf import csrf_exempt
+# for sending response to the client
+from django.http import HttpResponse, JsonResponse
+
+# Create your views here.
+
