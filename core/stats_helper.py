c3dbca512579e1df044ed0ee172972fafafc71a6
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2023-01-19 14:46:56 +0100

    #18 adding stats for each object + correcting indent in program item fetching

--- core/stats_helper.py
+++ core/stats_helper.py
@@ -1,7 +1,7 @@
 from .stat_models import Action
 from datetime import date
 def store_user_action(user, action):
-    if(user == ""):
+    if(user == "" or user is None):
         user = 'Unlogged'
     c_date = date.today()
     action = Action(user=user, date=c_date, action=action)
